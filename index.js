8782f5649724ffe4a88cc62ced1e2adc1a3c97c7
Justine B <justinegayton@gmail.com>
2019-03-04 22:10:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logDriversByHometown(drivers, hometown) {
 }
 
 function driversByRevenue(drivers) {
-  newArray = drivers;
+  newArray = drivers
   return newArray.sort(function(a, b) {
     return a.revenue - b.revenue;
   });
