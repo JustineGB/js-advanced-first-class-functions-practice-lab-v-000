8824310ea28641f858e90bde5548c976bac0e882
Justine B <justinegayton@gmail.com>
2019-03-04 22:32:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ function driversByName(drivers) {
 }
 
 function totalRevenue(drivers) {
-  return drivers.reduce(function(total, driver))
+  return drivers.reduce(function(total, driver) {
+    return driver.revenue + total}, 0);
 }
 
 function averageRevenue(drivers) { 
+  return totalRevenue
 }
