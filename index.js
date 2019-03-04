f8e7663db24ee90dead39cf7d49802231a27f376
Justine B <justinegayton@gmail.com>
2019-03-04 21:43:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,16 @@ function logDriverByHometown(drivers, location) {
 }
 
 
-// driversByRevenue() — Receives an array of driver objects and returns a new array of driver objects sorted by their revenue attribute from lowest to highest.
+// driversByRevenue() — Receives an array of driver objects 
+and returns a new array of driver objects sorted 
+by their revenue attribute from lowest to highest.
 
-// driversByName() — Receives an array of driver objects and returns a new array of driver objects sorted by their name attribute from A to Z. Here, you may have to use the String.prototype.localeCompare() method.
+// driversByName() — Receives an array of driver objects and 
+returns a new array of driver objects sorted by their name 
+attribute from A to Z. Here, you may have to use the String.prototype.localeCompare() method.
 
-// totalRevenue() — Receives an array of driver objects and returns the sum of the revenue earned by each driver.
+// totalRevenue() — Receives an array of driver objects and 
+returns the sum of the revenue earned by each driver.
 
-// averageRevenue() — Receives an array of driver objects and returns the average revenue earned by each driver
+// averageRevenue() — Receives an array of driver objects and 
+returns the average revenue earned by each driver
