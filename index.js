57ed17d04cc2b9d088a88a89ba18a511504e53e5
Justine B <justinegayton@gmail.com>
2019-03-04 22:43:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ function driversByName(drivers) {
 }
 
 function totalRevenue(drivers) {
-  const reducer = (accumulator, currentValue) => accumulator + currentValue;
   return drivers.reduce(function(total, driver) {
     return driver.revenue + total}, 0);
 }
