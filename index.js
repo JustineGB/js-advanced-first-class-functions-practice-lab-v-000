f1ee144ceb656924bd9d754d0fdb3f75c1a4429e
Justine B <justinegayton@gmail.com>
2019-03-04 21:53:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function driversByRevenue() {
 // by their revenue attribute from lowest to highest.
 }
 
-// driversByName() — Receives an array of driver objects and 
+function driversByName() {
+  // Receives an array of driver objects and 
 // returns a new array of driver objects sorted by their name 
 // attribute from A to Z. Here, you may have to use the String.prototype.localeCompare() method.
+}
 
-// totalRevenue() — Receives an array of driver objects and 
+function totalRevenue() — Receives an array of driver objects and 
 // returns the sum of the revenue earned by each driver.
 
-// averageRevenue() — Receives an array of driver objects and 
+function averageRevenue() — Receives an array of driver objects and 
 // returns the average revenue earned by each driver
