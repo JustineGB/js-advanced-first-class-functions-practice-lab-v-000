2e3e813b9639c3689e250b4faa799d1685ebdef1
Justine B <justinegayton@gmail.com>
2019-03-04 21:57:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ function logDriverNames(drivers) {
 
 function logDriverByHometown(drivers, hometown) {
   return drivers.forEach(function(driver) {
-    console.log(driver.name).where(driver.hometown == hometown);})
+    if driver.hometown == hometown {
+    console.log(driver.name)}
+  })
 }
 
 function driversByRevenue() {
